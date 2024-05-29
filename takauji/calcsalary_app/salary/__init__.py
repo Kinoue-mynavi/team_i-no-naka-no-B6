1e7256a5d6e79061b9290f3664762eb4467aacfa
takaujiji <takauji.tomone.qv@mynavi.jp>
2024-05-29 01:18:07 +0000

    edit calcsalary_app

--- takauji/calcsalary_app/salary/__init__.py
+++ takauji/calcsalary_app/salary/__init__.py
@@ -3,3 +3,5 @@ from flask import Flask
 app=Flask(__name__)
 import salary.views
 app.config.from_object('salary.config')
+
+from salary import views
--- takauji/calcsalary_app/salary/views.py
+++ takauji/calcsalary_app/salary/views.py
@@ -1,6 +1,12 @@
 from flask import request, redirect,url_for,render_template,flash,session
 from salary import app
+from decimal import Decimal,ROUND_HALF_UP
 
-@app.route('/')
-def show_entries():
-    return render_template("input.html")
+@app.route('/', methods['GET','POST'])
+def input():
+    input_data=session.get('input_data',None)
+    return render_template("input.html",input=input_data)
+
+@app.route('/output',methods=['GET','POST'])
+def output():
+    session["input_data"]=""
